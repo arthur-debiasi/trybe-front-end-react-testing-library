a15ad364042232c85beb5aab3a42fda35d505c0f
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-09-19 14:34:17 -0300

    initial commit :rocket:

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-);
+); // arthurdebiasicopyright
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
